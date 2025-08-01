730e07bb979d4e8a51c70b5a9eafca97d7a1d957
pychama1 <pychama1@cvut.cz>
2025-08-01 13:00:18 +0200

    Hopefully better result

--- txt_parser.py
+++ txt_parser.py
@@ -11,6 +11,7 @@ def add_header(file_location):
     df.columns = new_header  # assign as column names
     df.dropna(subset=['Equation'], inplace=True)
     # Save the new file (overwrite or save separately)
+    df = df[~df['ID'].astype(str).str.contains('net', case=False, na=False)]
     df.to_excel("A4GALT_flux_withHeader.xlsx", index=False)
 
 #add_header("./metabolomic_optimization/A4GALT_flux.xlsx")
@@ -18,15 +19,25 @@ def add_header(file_location):
 def read_txt_reaction_file(file_location):
     reactions_dict = {}
     with open(file_location) as file:
-        reactions = [line.rstrip() for line in file]        
+        reactions = [line.rstrip() for line in file]   
+    print(f"Number of reactions parsed from .txt file: {len(reactions)}")     
     for original_react in reactions:
         react = re.sub(r'\s*\([^)]*\)', '', original_react)
         react = react.split(';')[0]
         react = react.strip("'\"").strip()
+        
+        if react in reactions_dict:
+            print(f"FOR the reaction {react}, there are two reactions:")
+            print(f"First one: {reactions_dict[react]}")
+            print(f"Second one: {original_react}")
         reactions_dict[react] = original_react
+    print(f"Number of reactions in reactions dict: {len(reactions_dict)}")
+
     return reactions_dict
 
 def keep_only_relevant(reactions_dict, pruned_reactions, all_reactions, output_file_path):
+    print(f"Length of pruned reactions: {len(pruned_reactions)}")
+    
     reactions_to_write = list()
     already_added = set()
     #print("Writing the reactions:")
@@ -49,10 +60,17 @@ def keep_only_relevant(reactions_dict, pruned_reactions, all_reactions, output_f
     #for r in all_reactions:
     #    all_reactions_equations.add(r.equation)
     #print(f"{len(pruned_reactions_equations)}")
+    
+    
     reactions_dict_keys = set(reactions_dict.keys())
+    print(f"Length of equation to write: {len(reactions_to_write)}")
     with open(output_file_path, 'w') as file:
         for equation in reactions_to_write:
             file.write('%s\n' %equation)    
+    with open(output_file_path, 'r') as file:
+        line_count = sum(1 for line in file)
+
+    print(f'Total lines: {line_count}')
     print("Succesfully written the result into the file!")
     
 def run_the_script(pruned_reactions, all_reactions, input_file_path, output_file_path):
